cf6d935810ba5e57a8eef12a0062cd1ac698b5f8
Vijay Thangavel <vthangavel@sapient.com>
2015-08-29 00:52:59 +0530

    Server side rendering fixes.

--- server.js
+++ server.js
@@ -8,12 +8,13 @@ var AppComponent = React.createFactory(require('./js/components/ShoppingCart.rea
 
 expressServer.use('/static', express.static('static'));
 
+expressServer.set('view engine', 'ejs');
+
 expressServer.get('/', function(req, res, next){
     var component = AppComponent();
     var html = React.renderToString(component);
-    res.send(html);
+    res.render('index.ejs', {reactOutput: html});
 });
-
 
 expressServer.listen(port, function(){
     console.log('Server running in port 3000');
